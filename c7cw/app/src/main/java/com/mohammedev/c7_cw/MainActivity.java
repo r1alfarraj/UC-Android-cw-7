a9725a67c98e5a8fbd48cc1846cc062b946a907d
Mohammed Mansour <66431210+MohammedMansour64@users.noreply.github.com>
2022-06-14 20:25:40 +0300

    Add files via upload

---
+++ c7cw/app/src/main/java/com/mohammedev/c7_cw/MainActivity.java
@@ -0,0 +1,23 @@
+package com.mohammedev.c7_cw;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    ArrayList<Pokemon> pokemonList = new ArrayList<>();
+    ListView listView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        listView = findViewById(R.id.listview);
+
+        /** put Adapter Object and listView.setAdapter(); inside the volley function. **/
+
+
+    }
+}
